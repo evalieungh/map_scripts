46e36e78c117be047777211f2e2f2b3c4b4530b1
evalieungh <evaleriksen@gmail.com>
2023-07-12 11:21:47 +0200

    update incline maps

--- INCLINE-sites/study_area_maps.R
+++ INCLINE-sites/study_area_maps.R
@@ -19,31 +19,7 @@ library(raster)
 library(sf)
 }
 
-setwd('C:/Users/evaler/OneDrive - Universitetet i Oslo/Eva/PHD/Innlevering/')
-
-# specify site coordinates
-site_lat <- c(61.0243,60.8231,60.8328,60.9335)
-site_lon <- c(8.12343,7.27596,7.17561,6.41504)
-site_code <- c("ALP1","ALP2","ALP3","ALP4")
-site_name <- c("Ulvehaugen", "Lavisdalen", "Gudmedalen", "Skjellingahaugen")
-
-pointdata <- data.frame(site_lat,
-                   site_lon,
-                   site_code,
-                   site_name)
-
-# get extent of study region as square polygon by specifying corner points
-polygon_points <- st_polygon(list(rbind(
-  c(min(site_lon), min(site_lat)),
-  c(max(site_lon), min(site_lat)),
-  c(max(site_lon), max(site_lat)),
-  c(min(site_lon), max(site_lat)),
-  c(min(site_lon), min(site_lat))
-)))
-
-print(polygon_points)
-st_crs(polygon_points) <- 4326
-st_set_crs(polygon_points, 4326)
+setwd('C:/Users/evaler/OneDrive - Universitetet i Oslo/Eva/PHD/Innlevering/kappe/')
 
 # 1. Study area in N Europe
 # -------------------------------------------
@@ -56,7 +32,7 @@ europe <- ne_countries(scale='medium',
 fig1_a <- ggplot(data = europe) +
   geom_sf(fill = "darkgrey") +
   coord_sf(xlim = c(-5, 25), 
-           ylim = c(47, 65)) +
+           ylim = c(49, 65)) +
   xlab('') + ylab('') +
   # mark region with broader extent coordinates
   geom_rect(aes(
@@ -67,51 +43,79 @@ fig1_a <- ggplot(data = europe) +
   ),
   colour = "red",
   fill = NA)
-# 
-# # plot map with box around area
-# fig1_a <- ggplot(data = europe) +
-#   geom_sf(fill = "darkgrey",
-#           size = 1) +
-#   geom_polygon(data = polygon_points, 
-#                aes(),
-#                color = "red") +
-#   coord_sf(xlim = c(-6, 23),
-#            ylim = c(45, 70)) +
-#   xlab('') + ylab('') +
-#   theme_minimal() +
-#   theme(axis.text = element_blank()
-#   )
 fig1_a
 
 
 
 # 4. Sites in SW Norway
 # -------------------------------------------
+# download background map 
+norway <- ne_countries(scale ='large', 
+                       country = "Norway",
+                       returnclass = 'sf')
 
-#geom_point(
-#  data = alp4_coords,
-#  aes(x = longitude, y = latitude),
-#  size = 1,
-#  shape = 19,
-#  color = "red"
-#) +
+# specify site coordinates
+x <- c(8.12343,7.27596,7.17561,6.41504)
+y <- c(61.0243,60.8231,60.8328,60.9335)
+name <- c("Ulvehaugen", "Lavisdalen", "Gudmedalen", "Skjellingahaugen")
+
+pointdata <- data.frame(x,
+                        y,
+                        name)
+
+# set some axis parameters
+tick_positions <- c(5.7, 6, 6.5, 7, 7.5, 8, 8.5)
+tick_label_breaks <- c(6, 7, 8)
+
+# plot
+(fig1_b <-
+    # plot background map
+    ggplot(data = norway) +
+    geom_sf(fill = "darkgrey") +
+    # zoom in on region
+    coord_sf(xlim = c(5.5, 8.4),
+             ylim = c(60.3, 61.5)) +
+    # scale_x_continuous(
+    #   breaks = tick_label_breaks,
+    #   sec.axis = sec_axis( ~ ., breaks = tick_positions, labels = "")
+    # ) +
+    xlab('') + ylab('') +
+    # add site points
+    geom_point(
+      data = pointdata,
+      aes(x = x,
+          y = y,
+          group = name),
+      pch = 21,
+      color = "black",
+      fill = "red"
+    ) +
+    ggrepel::geom_text_repel(
+      data = pointdata,
+      aes(x = x,
+          y = y,
+          label = name))
+)
 
 # combine the maps
 # -------------------------------------
 
-composite_plot <- plot_grid(fig1_a, 
-                            fig1_b,
-                            labels = c('a', 'b'),
-                            nrow = 1, ncol = 2,
-                            #rel_widths = c(1, 1, 0.5),
-                            align = "hv")
+(fig1 <- plot_grid(
+  fig1_a,
+  fig1_b,
+  labels = c('a', 'b'),
+  nrow = 1,
+  ncol = 2,
+  #rel_widths = c(1, 1.1),
+  align = "hv"
+))
 
 
 # save the figure
 ggsave("figure_1.jpeg",
-       plot = fig1_map,
+       plot = fig1,
        device = "jpeg",
-       width = 500, 
-       height = 750,
+       width = 2000, 
+       height = 1000,
        dpi = "print",
        units = "px")
